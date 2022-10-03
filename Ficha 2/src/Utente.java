d33fd619e07721356c62e73f022942e5e23fccbc
Ivo Gomes <Ivo Gomes@MSI-IvoGomes>
2022-10-03 14:51:47 +0100

    Versao 2

--- Ficha 2/src/Utente.java
+++ Ficha 2/src/Utente.java
@@ -47,4 +47,101 @@ public class Utente {
 			return limite_peso;
 		}
 
+		/**
+		 * @return the nome
+		 */
+		public String getNome() {
+			return nome;
+		}
+
+		/**
+		 * @param nome the nome to set
+		 */
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+		/**
+		 * @return the genero
+		 */
+		public String getGenero() {
+			return genero;
+		}
+
+		/**
+		 * @param genero the genero to set
+		 */
+		public void setGenero(String genero) {
+			this.genero = genero;
+		}
+
+		/**
+		 * @return the idade
+		 */
+		public int getIdade() {
+			return idade;
+		}
+
+		/**
+		 * @param idade the idade to set
+		 */
+		public void setIdade(int idade) {
+			this.idade = idade;
+		}
+
+		/**
+		 * @return the altura
+		 */
+		public float getAltura() {
+			return altura;
+		}
+
+		/**
+		 * @param altura the altura to set
+		 */
+		public void setAltura(float altura) {
+			this.altura = altura;
+		}
+
+		/**
+		 * @return the peso
+		 */
+		public int getPeso() {
+			return peso;
+		}
+
+		/**
+		 * @param peso the peso to set
+		 */
+		public void setPeso(int peso) {
+			this.peso = peso;
+		}
+
+		/**
+		 * @return the iMC
+		 */
+		public double getIMC() {
+			return IMC;
+		}
+
+		/**
+		 * @param iMC the iMC to set
+		 */
+		public void setIMC(double iMC) {
+			IMC = iMC;
+		}
+
+		/**
+		 * @return the limite_peso
+		 */
+		public String getLimite_peso() {
+			return limite_peso;
+		}
+
+		/**
+		 * @param limite_peso the limite_peso to set
+		 */
+		public void setLimite_peso(String limite_peso) {
+			this.limite_peso = limite_peso;
+		}
 }
